81e557468ee04f820caa1a32c1232ea70f8e51b1
David Sanders <david.wm.sanders@gmail.com>
2023-01-31 02:47:28 +0000

    Add basic tracing

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,32 @@
 //     println!("Hello, world!");
 // }
 
-use axum::{response::Html, routing::get, Router};
 use std::net::SocketAddr;
+
+// use tracing::debug;
+use tracing_subscriber::{layer::SubscriberExt,util::SubscriberInitExt};
+use axum::{response::Html, routing::get, Router};
+use tower_http::trace::TraceLayer;
+
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    // setup tracing subscriber first and foremost
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "marshalrwr=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // build our application with a route and add the tower-http tracing layer
+    let app = Router::new()
+        .route("/", get(handler)).layer(TraceLayer::new_for_http());
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
+    tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await

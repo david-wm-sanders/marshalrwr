05b4046a45a899e38c2a926519d851827f17294c
David Sanders <david.wm.sanders@gmail.com>
2023-02-22 02:39:03 +0000

    Add generated player entity

--- src/entity/src/lib.rs
+++ src/entity/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod prelude;
 pub mod realm;
+pub mod player;
 
 pub use prelude::Realm;
 pub use realm::{Model as RealmModel, ActiveModel as RealmActiveModel, Column as RealmColumn};
+pub use prelude::Player;
+pub use player::{Model as PlayerModel, ActiveModel as PlayerActiveModel, Column as PlayerColumn};

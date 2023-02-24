9e3dcbb96fa4501a3a3909237c96d84fc0b7ebb1
David Sanders <david.wm.sanders@gmail.com>
2023-02-24 11:43:52 +0000

    Tidy

--- src/app/profile_server/get.rs
+++ src/app/profile_server/get.rs
@@ -7,20 +7,16 @@ use sea_orm::{EntityTrait, QueryFilter, ColumnTrait, error::DbErr};
 // use crate::app::errors::ServerError;
 use super::errors::ProfileServerError;
 use super::super::{state::AppState, validated_query::ValidatedQuery};
-use super::util::{digest_ok, get_realm, get_player_from_db, get_account_from_db};
+use super::util::{check_realm_is_configured, digest_ok, get_realm, get_player_from_db, get_account_from_db};
 
 use super::params::GetProfileParams;
 
+
+
 #[debug_handler]
 pub async fn rwr1_get_profile_handler(State(state): State<AppState>, ValidatedQuery(params): ValidatedQuery<GetProfileParams>) -> Result<Html<String>, ProfileServerError> {
-    // check that this realm is in state.config, this acts as a guard whilst the realm digest algo remains a mystery
-    // as we cannot derive the digest from knowing the realm secret and pw, the server expects the realms to be named (e.g. ["INCURSION"]) in the config instead
-    // when the first request for a realm is received, it will be created in the db with the digest supplied in the first request
-    // this should be fine when the IP allowlist for the profile server endpoints is implemented
-    if !state.config.realms.iter().any(|realm_name| realm_name == &params.realm) {
-        tracing::error!("realm '{}' not configured", &params.realm);
-        return Err(ProfileServerError::RealmNotConfigured(String::from(&params.realm)));
-    }
+    // check that the realm has been configured, see func comments for more detail
+    check_realm_is_configured(&state, &params.realm)?;
     
     // get the realm
     let realm = get_realm(&state, &params.realm, &params.realm_digest).await?;
@@ -30,18 +26,7 @@ pub async fn rwr1_get_profile_handler(State(state): State<AppState>, ValidatedQu
         return Err(ProfileServerError::RealmDigestIncorrect(String::from(&params.realm), String::from(&params.realm_digest)));
     }
 
-    // todo: find player in db, if not exist make and send init profile xml
-    // if let Some(p) = get_player_from_db(&state.db, params.hash).await? {
-    //     tracing::debug!("found player '{}' in db, verifying rid :eyes:", p.username);
-    // } else {
-    //     tracing::info!("player '{}' not found in db, enlisting them...", &params.username);
-    // }
-
     tracing::info!("checking db for player '{}' in '{}' realm", &params.username, &params.realm);
-    // get an optional player and optional account, then match on Some|None to flow to logic for:
-    // (None<Player>, None<Account>) - create player and then init profile for player in realm
-    // (Some<Player>, None<Account>) - player was created (by get) previously but no set, resend init
-    // (Some<Player>, Some<Account>) - the player has some account for this realm, send it to them
     let opt_player = get_player_from_db(&state.db, params.hash).await?;
     let opt_account = get_account_from_db(&state.db, realm.id, params.hash).await?;
     match (opt_player, opt_account) {
--- src/app/profile_server/util.rs
+++ src/app/profile_server/util.rs
@@ -8,12 +8,38 @@ use entity::{Realm, RealmModel, RealmActiveModel, RealmColumn};
 use entity::{Player, PlayerModel, PlayerActiveModel, PlayerColumn};
 use entity::{Account, AccountModel, AccountActiveModel, AccountColumn};
 
+pub fn check_realm_is_configured(state: &AppState, realm: &str) -> Result<(), ProfileServerError> {
+    // check that this realm is in state.config, this acts as a guard whilst the realm digest algo remains a mystery
+    // as we cannot derive the digest from knowing the realm secret and pw, the server expects the realms to be named (e.g. ["INCURSION"]) in the config instead
+    // when the first request for a realm is received, it will be created in the db with the digest supplied in the first request
+    // this should be fine when the IP allowlist for the profile server endpoints is implemented
+    if !state.config.realms.iter().any(|realm_name| realm_name == realm) {
+        tracing::error!("realm '{}' not configured", realm);
+        return Err(ProfileServerError::RealmNotConfigured(String::from(realm)));
+    }
+    Ok(())
+}
+
 pub fn digest_ok(given_digest: &str, valid_digest: &str) -> bool {
     // check the realm digest in constant time mit subtle crate
     // todo: validate that this actually works in constant time XD
     let given_digest_bytes = given_digest.as_bytes();
     let valid_digest_bytes = valid_digest.as_bytes();
     given_digest_bytes.ct_eq(valid_digest_bytes).into()
+}
+
+pub async fn get_realm_from_db(db_conn: &DatabaseConnection, realm_name: &str) -> Result<Option<RealmModel>, DbErr> {
+    Ok(Realm::find().filter(RealmColumn::Name.eq(realm_name)).one(db_conn).await?)
+}
+
+pub async fn get_player_from_db(db_conn: &DatabaseConnection, player_hash: i64) -> Result<Option<PlayerModel>, DbErr> {
+    // get player by i64 hash id
+    Ok(Player::find_by_id(player_hash).one(db_conn).await?)
+}
+
+pub async fn get_account_from_db(db_conn: &DatabaseConnection, realm_id: i32, player_hash: i64) -> Result<Option<AccountModel>, DbErr> {
+    // get the account by (realm_id, player_hash)
+    Ok(Account::find_by_id((realm_id, player_hash)).one(db_conn).await?)
 }
 
 pub async fn get_realm(state: &AppState, realm_name: &str, realm_digest: &str) -> Result<RealmModel, ProfileServerError> {
@@ -59,21 +85,7 @@ pub async fn get_realm(state: &AppState, realm_name: &str, realm_digest: &str) -
     }
 }
 
-pub async fn get_realm_from_db(db_conn: &DatabaseConnection, realm_name: &str) -> Result<Option<RealmModel>, DbErr> {
-    Ok(Realm::find().filter(RealmColumn::Name.eq(realm_name)).one(db_conn).await?)
-}
-
-pub async fn get_player_from_db(db_conn: &DatabaseConnection, player_hash: i64) -> Result<Option<PlayerModel>, DbErr> {
-    // get player by i64 hash id
-    Ok(Player::find_by_id(player_hash).one(db_conn).await?)
-}
-
 pub async fn get_player_from_db_by_name(db_conn: &DatabaseConnection, username: &str) -> Result<Option<()>, DbErr> {
     todo!();
     Ok(None)
-}
-
-pub async fn get_account_from_db(db_conn: &DatabaseConnection, realm_id: i32, player_hash: i64) -> Result<Option<AccountModel>, DbErr> {
-    // get the account by (realm_id, player_hash)
-    Ok(Account::find_by_id((realm_id, player_hash)).one(db_conn).await?)
 }

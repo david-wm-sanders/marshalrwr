4ad48067e5150eb5006de07a276c2df1b4b343fb
David Sanders <david.wm.sanders@gmail.com>
2023-02-25 19:28:51 +0000

    Tidy and try to return init profile

--- src/app/profile_server/get.rs
+++ src/app/profile_server/get.rs
@@ -1,3 +1,5 @@
+use axum::http::{StatusCode, header};
+use axum::response::{Response, IntoResponse};
 use axum::{extract::State, response::Html};
 use axum_macros::debug_handler;
 
@@ -9,28 +11,15 @@ use super::util::{check_realm_is_configured, get_realm, get_player, enlist_playe
 use super::params::GetProfileParams;
 
 #[debug_handler]
-pub async fn rwr1_get_profile_handler(State(state): State<AppState>, ValidatedQuery(params): ValidatedQuery<GetProfileParams>) -> Result<Html<String>, ProfileServerError> {
+pub async fn rwr1_get_profile_handler(State(state): State<AppState>, ValidatedQuery(params): ValidatedQuery<GetProfileParams>) -> Result<Response, ProfileServerError> {
+    let headers  = [(header::CONTENT_TYPE, "text/xml")];
+
     // check that the realm has been configured, see fn comments for more detail
     check_realm_is_configured(&state, &params.realm)?;
 
     // get the realm, making it if it doesn't exist yet
     tracing::info!("locating realm '{}'", &params.realm);
     let realm = get_realm(&state, &params.realm, &params.realm_digest).await?;
-
-    // match (opt_player, opt_account) {
-    //     (None, None) => {
-    //         tracing::info!("player '{}' not found in db, enlisting them (pending checks)", &params.username);
-    //         // todo: run complex validation on username here :D
-    //         // todo: create player and then init profile for player in realm
-    //     },
-    //     (Some(player), None) => {
-    //         // todo: player was created (by get) previously but no set, resend init
-    //     },
-    //     (Some(player), Some(account)) => {
-    //         // todo: the player has some account for this realm, send it to them
-    //     }
-    //     (None, Some(_)) => unreachable!("no player but some account wtf")
-    // }
 
     // find the player, if any
     tracing::info!("finding enlistment papers for player '{}'", &params.username);
@@ -43,7 +32,8 @@ pub async fn rwr1_get_profile_handler(State(state): State<AppState>, ValidatedQu
             // make an initialisation profile for the player
             let init_profile_xml = make_init_profile_xml(&player.username, &player.rid)?;
             tracing::debug!("sending init profile: '{init_profile_xml}' to game server");
-            // todo: return Xml response
+            // return xml response
+            return Ok((StatusCode::OK, headers, init_profile_xml).into_response());
         },
         Some(player) => {
             tracing::debug!("Some player");
@@ -52,5 +42,6 @@ pub async fn rwr1_get_profile_handler(State(state): State<AppState>, ValidatedQu
     }
 
     let s = format!("{params:#?} {state:#?}");
-    Ok(Html(s))
+    // Ok(Html(s))
+    Ok((StatusCode::OK, [(header::CONTENT_TYPE, "text/plain")], s).into_response())
 }

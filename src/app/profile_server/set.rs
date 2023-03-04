8af78bfa43c60f0ab00ccebd299b428c3b589187
David Sanders <david.wm.sanders@gmail.com>
2023-03-04 20:21:47 +0000

    Start detailing structs for set profile xml deserialisation

--- src/app/profile_server/set.rs
+++ src/app/profile_server/set.rs
@@ -41,10 +41,9 @@ where
         let xml_str = std::str::from_utf8(&body_vec).unwrap();
         // tracing::debug!("{xml_str:#?}");
         let decoded_xml_str = percent_decode_str(xml_str).decode_utf8().unwrap();
-        tracing::debug!("{decoded_xml_str:#?}");
+        // tracing::debug!("{decoded_xml_str:#?}");
         let data = quick_xml::de::from_str(decoded_xml_str.as_ref())?;
         // do validation...
-        // let data: T = body.into();
         Ok(Self(data))
     }
 }
@@ -64,16 +63,101 @@ where
 // }
 
 #[derive(Debug, Deserialize)]
+pub struct SetProfileDataXml {
+    #[serde(rename = "player")]
+    players: Vec<PlayerXml>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct PlayerXml {
     #[serde(rename = "@hash")]
     hash: i64,
     #[serde(rename = "@rid")]
-    rid: String
+    rid: String,
+    person: PersonXml,
+    profile: ProfileXml,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct SetProfileDataXml {
-    player: Vec<PlayerXml>
+pub struct PersonXml {
+    #[serde(rename = "@max_authority_reached")]
+    max_authority_reached: f32,
+    #[serde(rename = "@authority")]
+    authority: f32,
+    #[serde(rename = "@job_points")]
+    job_points: f32,
+    #[serde(rename = "@faction")]
+    faction: i32,
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@soldier_group_id")]
+    soldier_group_id: i32,
+    #[serde(rename = "@soldier_group_name")]
+    soldier_group_name: String,
+    #[serde(rename = "@squad_size_setting")]
+    squad_size_setting: i32,
+    // todo: add loadout, stash, and backpack
+    #[serde(rename = "item")]
+    equipped_items: Vec<EquippedItemXml>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct EquippedItemXml {
+    #[serde(rename = "@slot")]
+    slot: i32,
+    #[serde(rename = "@index")]
+    index: i32,
+    #[serde(rename = "@amount")]
+    amount: i32,
+    #[serde(rename = "@key")]
+    key: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ProfileXml {
+    #[serde(rename = "@game_version")]
+    game_version: i32,
+    #[serde(rename = "@username")]
+    username: String,
+    #[serde(rename = "@sid")]
+    sid: i64,
+    #[serde(rename = "@rid")]
+    rid: String,
+    #[serde(rename = "@squad_tag")]
+    squad_tag: String,
+    // #[serde(rename = "@")]
+    stats: StatsXml,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct StatsXml {
+    #[serde(rename = "@kills")]
+    kills: i32,
+    #[serde(rename = "@deaths")]
+    deaths: i32,
+    #[serde(rename = "@time_played")]
+    time_played: f32,
+    #[serde(rename = "@player_kills")]
+    player_kills: i32,
+    #[serde(rename = "@teamkills")]
+    teamkills: i32,
+    #[serde(rename = "@longest_kill_streak")]
+    longest_kill_streak: i32,
+    #[serde(rename = "@targets_destroyed")]
+    targets_destroyed: i32,
+    #[serde(rename = "@vehicles_destroyed")]
+    vehicles_destroyed: i32,
+    #[serde(rename = "@soldiers_healed")]
+    soldiers_healed: i32,
+    #[serde(rename = "@distance_moved")]
+    distance_moved: f32,
+    #[serde(rename = "@shots_fired")]
+    shots_fired: i32,
+    #[serde(rename = "@throwables_thrown")]
+    throwables_thrown: i32,
+    #[serde(rename = "@rank_progression")]
+    rank_progression: f32,
+    // todo: add monitors
 }
 
 #[debug_handler]
